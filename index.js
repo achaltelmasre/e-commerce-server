f971bac0916e9b67a70148b0b63c84fc53841729
achaltelmasre <achaltelmasre@gmail.com>
2023-10-22 16:06:27 +0530

    added delete, put, patch request

--- index.js
+++ index.js
@@ -100,6 +100,117 @@ app.get('/product', async (req, res) => {
     })
  });
 
+ //Delete request
+app.delete('/product/:_id', async (req,res) =>{
+    const { _id } = req.params;
+
+     await Product.deleteOne({ _id:  _id});
+
+    res.json({
+        success:true,
+        data: {},
+        message:`Successfully deleted product with id ${_id}  `,
+    })
+});
+
+//put student
+app.put('/product/:_id', async (req, res) => {
+    const { _id } = req.params;
+    const { name,  description,  price, productimg, brand } = req.body;
+   
+    if (!name) {
+       return res.json({
+             success:false,
+             message: 'Name is required',
+         })
+     }
+   
+     if (!description) {
+         return res.json({
+               success:false,
+               message: 'Description is required',
+           })
+       }
+   
+       if (!price) {
+         return res.json({
+               success:false,
+               message: 'Price is required',
+           })
+       }
+   
+       if (!productimg) {
+         return res.json({
+               success:false,
+               message: 'Productimg is required',
+           })
+       }
+   
+       if (!brand) {
+         return res.json({
+               success:false,
+               message: 'Brand is required',
+           })
+       }
+   
+      await Product.updateOne(
+   
+       {_id:_id},
+       {$set: {
+           name: name,
+           description: description,
+           price: price,
+           productimg: productimg,
+           brand: brand,
+       }})
+   
+       const updatedProduct = await Product.findOne({_id:_id});
+   
+       res.json({
+           success:true,
+           data: updatedProduct,
+           message: `Successfully update `
+       })
+ });
+
+ //patch request
+ app.patch('/product/:_id', async (req, res) =>{
+    const { _id } = req.params;
+    const { name, description, price, productimg, brand  } = req.body;
+
+    const product = await Product.findById(_id);
+
+    if(name){
+        product.name = name;
+    }
+
+    if(description){
+        product.description = description;
+    }
+
+    if(price){
+        product.price = price;
+    }
+
+    if(productimg){
+        product.productimg = productimg;
+    }
+
+    if(brand){
+        product.brand  = brand;
+    }
+
+    const updatedProduct = await product.save();
+
+  res.json({
+    success:true,
+    data:updatedProduct,
+    message:"update successfully"
+  })
+
+});
+
 app.listen(PORT, () =>{
     console.log(` Server running on port ${PORT}`);
 });
+
